fd47d190a6f380c2c5a3b2fed4852b1a87e33c51
David Charles <ratlover03@gmail.com>
2019-10-01 00:48:27 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,9 @@
 class Dog
   attr_accessor :name, :breed, :age
-
+  all = []
     def initialization(name, breed, age)
       @name = name
       @breed = breed
-      @age = age 
+      @age = age
     end
 end
